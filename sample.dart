a185fa2038b227acc5e46d3ff3f70840a8f6054c
Alekiie <amnjogu78@gmail.com>
2024-03-23 09:11:29 +0300

    sample

---
+++ sample.dart
@@ -0,0 +1,12 @@
+import 'dart:io';
+
+void main() {
+  // Prompt the user for input
+  stdout.write("Please enter some text: ");
+
+  // Read the input from the user
+  String? input = stdin.readLineSync();
+
+  // Print the user's input
+  // print("You entered: $input");
+}
